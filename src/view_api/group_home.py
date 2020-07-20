4a406b4fe5cb9a7765d597101bf82ae63bf1cf10
deveshdatwani <datwanidevesh@gmail.com>
2020-07-20 20:29:32 +0530

    added group view

---
+++ src/view_api/group_home.py
@@ -0,0 +1,50 @@
+import functools
+from flask import Blueprint
+from flask import request
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+from flask import jsonify
+
+bp = Blueprint("player_home", __name__)
+
+@bp.route("/player_home", methods=("GET", "POST"))
+def login():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        groupid = request.form['groupid']
+        player = None
+    
+    # ------------------------------------------------------------- 
+
+
+    #Add code to fire a query to the SQL database in order to retrieve user iformation from user's username
+    #Store the information in the object player
+
+
+    # ------------------------------------------------------------------------
+
+        
+        #Authenticate user credentials before returning user information
+
+        if player is None:
+            response= "User not found"
+        
+        elif not check_password_hash(player["password"], password):
+            response = 'Incorrect password'
+
+        elif check_password_hash(player['password'], password):
+            response = player
+
+
+            # --------------------------------------------------------------
+
+            #Add code to fire a query to the SQL database in order to retrieve details of ALL players in the group table mentioned
+            #Store the player details in variable called response
+
+
+            # --------------------------------------------------------------
+
+        
+    return 'Group Home'
