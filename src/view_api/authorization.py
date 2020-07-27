42210f8e147121d062639335c40b2111d11d1132
deveshdatwani <datwanidevesh@gmail.com>
2020-07-27 15:44:12 +0530

    adding database

--- src/view_api/authorization.py
+++ src/view_api/authorization.py
@@ -3,20 +3,30 @@ import mysql.connector
 
 def authorization(username, password):
 
-	connector = mysql.connector.connect(user='root',database='flex_play',host='localhost',password='flexplay')
-	
-	player = None
-	
-	if player is None:
-		response = "User not found"
-		access = False
-	
-	elif not check_password_hash(player["password"], password):
-		response = 'Incorrect password'
-		access = False
+	try:
+		connector = mysql.connector.connect(,host='localhost',user='root',password='flexplay',database='flexplay')
+		
+		if connector.is_connected():
+			cursor = connector.cursor()
+			Player = cursor.execute('SELECT * from players WHERE player_username = username')
+			
+			if Player is not None:	
+				player_info = cursor.fetchone()
+				cursor.execute('SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "players"')
+				columns = cursor.fetchone() 
+				player_info_tuple = zip(columns, player_info)
+				player = dict(player_info_tuple)
 
-	elif check_password_hash(player['password'], password):
-		response = player 
-		access = True
+		if Player is None:
+			response = "User not found"
+			access = False
+	
+		elif not check_password_hash(player["password"], password):
+			response = 'Incorrect password'
+			access = False
+
+		elif check_password_hash(player['password'], password):
+			response = player 
+			access = True
 
 	return access, player
