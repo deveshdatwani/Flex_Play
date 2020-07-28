6fba6ec75d67fad4132d1ec5c55be187d0cb39af
deveshdatwani <datwanidevesh@gmail.com>
2020-07-29 04:16:20 +0530

    Addd sql connector to create new tables

--- src/view_api/create_event.py
+++ src/view_api/create_event.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
-from authorization import authorization
+import mysql.connector
 
 bp = Blueprint("create_event", __name__)
 
@@ -9,15 +9,23 @@ bp = Blueprint("create_event", __name__)
 def create_event():
 
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        creater = request.form["creater"]
         event_arena = request.form['eventarena']
         location = request.form['location']
+        team = request.form['team']
+        players = request.form['players']
+        timeslot = request.form['timeslot']
+        connector = mysql.connector.connect(host='localhost',user='root',password='flexplay',database='flexplay')
+    
+        if connector.is_connected():
+            cursor = connector.cursor(buffered=True)
+            query = "INSERT INTO events_master (creater, eventarena, location, team, players, timeslot) VALUES (%s, %s, %s, %s, %s, %s);" 
+            val = creater, event_arena, location, team, players, timeslot
+            cursor.execute(query, val)
+            connector.commit() 
+            response = 'Created Event Successfully'
+            cursor.close()
+            connector.close()
+       
 
-        #-----------------------------------------------------------------
-
-        #Add code to create a new 'Event' table in the SQL database
-
-        #-----------------------------------------------------------------
-
-    return new_event
+    return response
