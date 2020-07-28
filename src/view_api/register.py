b18088742da0b76e550bfa8089dff2306db8c792
deveshdatwani <datwanidevesh@gmail.com>
2020-07-29 04:18:49 +0530

    added sql code for inserting entry of new player

--- src/view_api/register.py
+++ src/view_api/register.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 from flask import flash
 from flask import request
 from werkzeug.security import generate_password_hash
+import mysql.connector
+from random import randint
 
 bp = Blueprint('register', __name__)
 
@@ -10,19 +12,26 @@ bp = Blueprint('register', __name__)
 def register():
 
     if request.method == "POST":
+        player_id = randint(10000000,99999999)
         username = request.form["username"]
-        password = request.form["password"]
-        
-    # ---------------------------------------------------------------------
-
-
-    #Create a user in the SQL database with the username and password in above variables
-    #Use generate_password_hash function to encrypt password
-
-
-    # --------------------------------------------------------------------------  
-
-        return 'Created Account Succesfully'
-
+        password = generate_password_hash(request.form["password"])
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        emailid = request.form['emailid']
+        phonenumber = request.form['phonenumber']
+        teamname = request.form['teamname']      
+        connector = mysql.connector.connect(host='localhost',user='root',password='flexplay',database='flexplay')
+    
+        if connector.is_connected():
+            cursor = connector.cursor(buffered=True)
+            query = "INSERT INTO players_master (player_id, username, password, firstname, lastname, emailid, phonenumber, teamname) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);" 
+            val = player_id, username, password, firstname, lastname, emailid, phonenumber, teamname
+            cursor.execute(query, val)
+            connector.commit() 
+            response = 'Created Account Successfully'
+            cursor.close()
+            connector.close()
     else:
-        return 'Hello'
+        repsone = 'Hello'
+
+    return response
