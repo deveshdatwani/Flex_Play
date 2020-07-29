7532350d7e1421ed6452b066f11445f4c659abde
deveshdatwani <datwanidevesh@gmail.com>
2020-07-29 22:42:35 +0530

    commiting without location parameter

--- src/view_api/match_making.py
+++ src/view_api/match_making.py
@@ -1,25 +1,38 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
-from random import choice
+import mysql.connector
 
 bp = Blueprint("match_making", __name__)
 
-def make_match(time_slot, location):
+def make_match(timeslot, location):
+	connector = mysql.connector.connect(host='localhost',user='root',password='flexplay',database='flexplay')
+	
+	if connector.is_connected():
+		cursor = connector.cursor()
+		query = 'SELECT * FROM events_master WHERE timeslot = %s;'
+		val = timeslot
+		cursor.execute(query, (val,))
+		events = cursor.fetchall()
 
-    # --------------------------------------------------------------
+	if events is None:
+		response = "No events found"
 
-    #Add code to fire a query to the SQL database in order to retrieve ALL events in the time slot store in the above variable
-    #Store the table primary keys in a list called events
+	else:
+		response = events
+	
+	cursor.close()
+	connector.close() 
 
-    # --------------------------------------------------------------
-
-    return events
+	return events
 
 @bp.route("/match_making", methods=("GET", "POST"))
-def login():
-        
-    events = make_match(time_slot, location)
-    matched_event = choice(events)
+def match_making():   
+	
+	if request.method == "POST":
+		location = request.form["location"]
+		timeslot = request.form["timeslot"]   
+    
+	events = make_match(timeslot, location)
             
-    return matched_event
+	return jsonify(events)
