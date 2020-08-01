66794681b0580ad79b5840b589b3147224b926a4
deveshdatwani <datwanidevesh@gmail.com>
2020-08-02 04:58:31 +0530

    username display from group

--- src/view_api/group_home.py
+++ src/view_api/group_home.py
@@ -1,20 +1,30 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
+import mysql.connector
 
 bp = Blueprint("group_home", __name__)
 
 @bp.route("/group_home", methods=("GET", "POST"))
 def login():
 
-    if request.method == "POST":
-        group_id = request.form['groupid']
-    
-        # --------------------------------------------------------------
+	if request.method == "POST":
+		username = request.form['username']
+		connector = mysql.connector.connect(host='localhost',user='root',password='flexplay',database='flexplay')
+	
+	if connector.is_connected():
+		cursor = connector.cursor()
+		query = 'SELECT teamname FROM players_master WHERE username = "' + username + '"'
+		cursor.execute(query)
+		teamname = cursor.fetchone()
+		teamname = teamname[0]
+		query = 'SELECT username FROM players_master WHERE teamname = "' + teamname + '"'
+		cursor.execute(query)
+		group = cursor.fetchall()
+		cursor.close()
+		connector.close()
+		group = [x[0] for x in group]
+		group = ','.join(group)
 
-        #Add code to fire a query to the SQL database in order to retrieve details of ALL players in the group table mentioned
-        #Store the player details in variable called group
 
-        # --------------------------------------------------------------
-   
-    return group
+	return group
