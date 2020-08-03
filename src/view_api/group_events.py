48ef356f146593f04e97fd6cf912782919d4f073
deveshdatwani <datwanidevesh@gmail.com>
2020-08-03 19:14:03 +0530

    joining and creating events

---
+++ src/view_api/group_events.py
@@ -0,0 +1,40 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+import mysql.connector
+import group_home
+
+bp = Blueprint("group_events", __name__)
+
+@bp.route("/group_events", methods=("GET", "POST"))
+def group_events():
+
+	if request.method == "POST":
+		username = request.form['username']
+		connector = mysql.connector.connect(host='localhost',user='root',password='flexplay',database='flexplay')
+	
+	if connector.is_connected():
+		cursor = connector.cursor()
+		query = 'SELECT teamname FROM players_master WHERE username = "' + username + '"'
+		cursor.execute(query)
+		teamname = cursor.fetchone()
+		teamname = teamname[0]
+		query = 'SELECT username FROM players_master WHERE teamname = "' + teamname + '"'
+		cursor.execute(query)
+		group = cursor.fetchall()
+		#cursor.close()
+		#connector.close()
+		group = [x[0] for x in group]
+		group = ','.join(group)
+		print(group)
+
+	if group is not None:
+		group = group.split(',')
+		
+		for player in group:
+			query = 'SELECT * FROM events_master WHERE creater = "' + player + '"'
+			cursor.execute(query)
+			event = cursor.fetchone()
+			print(event)
+
+	return 'Hey'
---
+++ src/view_api/join_event.py
@@ -0,0 +1,34 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+import mysql.connector
+
+bp = Blueprint("join_event", __name__)
+
+@bp.route('/create_event', methods=['GET','POST'])
+def join_event():
+
+    if request.method == "POST":
+        creater = request.form['creater']
+        player = request.form['player']
+        daytime = request.form['daytime']
+        eventarena = request.form['eventarena']
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        team = request.form['team']
+        privacy = request.form['privacy']
+        gameplaytime = request.form['gameplaytime']
+        connector = mysql.connector.connect(host='localhost',user='root',password='flexplay',database='flexplay')
+    
+        if connector.is_connected():
+            cursor = connector.cursor(buffered=True)
+            query = "INSERT INTO events_master(creater, player, daytime, eventarena, lat, lng, team, privacy, gameplaytime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);" 
+            val = creater, player, daytime, eventarena, latitude, longitude, team, privacy, gameplaytime
+            cursor.execute(query, val)
+            connector.commit() 
+            response = 'Joined The Event Successfully'
+            cursor.close()
+            connector.close()
+       
+
+    return response
