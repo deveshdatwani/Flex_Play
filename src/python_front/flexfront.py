dee26af969d11899806f575977f2b954d20d63b0
deveshdatwani <datwanidevesh@gmail.com>
2020-08-13 03:25:21 +0530

    adding friend notifications

--- src/python_front/flexfront.py
+++ src/python_front/flexfront.py
@@ -31,7 +31,7 @@ class MainWindow(Screen):
 		self.firstname = response.json()[1]['firstname']
 		self.email = response.json()[1]['email']
 		self.phonenumber = str(response.json()[1]['phonenumber'])
-		self.team = response.json()[1]['email']
+		self.team = response.json()[1]['teamname']
 		
 		return response.json()[0]['access']
 	pass
@@ -43,6 +43,8 @@ class PlayerHome(Screen):
 	email = StringProperty('')
 	phonenumber = StringProperty('')
 	groupplayers = ListProperty('')
+	yourevents = ListProperty('')
+	yournotifications = ListProperty('')
 
 	def go_to_group_home(self,username):
 
@@ -50,11 +52,26 @@ class PlayerHome(Screen):
 		obj = {'username':username}
 		response = requests.post(url=url, data = obj)
 		self.groupplayers = response.content.decode('utf-8').split(',')
-		print(type(self.groupplayers))
+
+	def see_your_events(self,username):
+
+		url = 'http://127.0.0.1:5000/your_events'
+		obj = {'username':username}
+		response = requests.post(url=url, data=obj)
+		self.yourevents = response.content
+
+	def see_your_notifications(self,username):
+
+		url = 'http://127.0.0.1:5000/see_your_notifications'
+		obj = {'username':username}
+		response = requests.post(url=url, data = obj)
+		notifications = response.content.decode('utf-8')
+		self.yournotifications = notifications.split('--')
 
 	pass
 
 class WindowManager(ScreenManager):
+	
 	pass 
 
 class GroupHome(Screen):
@@ -62,8 +79,10 @@ class GroupHome(Screen):
 	username = StringProperty('')
 	groupplayers = ListProperty('')
 	groupevents = ListProperty('')
+	yournotifications = ListProperty('')
 
 	def see_group_events(self,username):
+		
 		event_list = []
 		all_events = []
 		url = 'http://127.0.0.1:5000/group_events'
@@ -73,8 +92,9 @@ class GroupHome(Screen):
 		response = response.replace(',', '\t')
 		event_list =response.split('\t')
 		self.groupevents = [str(event_list[i:i+3]) for i in range(0,len(event_list),4)]
-		self.groupevents	
-	
+		
+		pass
+
 	pass
 
 class CreateGroupEvent(Screen):
@@ -106,6 +126,8 @@ class SeeGroupEvents(Screen):
 
 	groupevents = ListProperty('')
 	eventplayers = ListProperty('')
+	eventname = StringProperty('')
+	eventdate = StringProperty('')
 
 	@mainthread
 	def on_enter(self):
@@ -122,16 +144,13 @@ class SeeGroupEvents(Screen):
 		daytime = events[2]
 		creater = creater.split("'")[1]
 		daytime = daytime.split("'")[1]
-		self.manager.current = 'SeeEventDetails'
-
 		url = 'http://127.0.0.1:5000/see_event_details'
 		obj = {'creater':creater, 'daytime':daytime}
 		response = requests.post(url=url, data = obj)
-		self.eventplayers = response.content.split(",")
-
-		return self.eventplayers
-
-	self.eventplayers = ['jasman']
+		self.eventplayers = response.content.decode('utf-8').split(',')
+		self.manager.current = 'SeeEventDetails'
+		self.eventname = creater + "'s " + 'Event'
+		self.eventdate = "Event Date: " + daytime
 	
 	pass
 
@@ -167,6 +186,44 @@ class FindEvents(Screen):
 class SeeEventDetails(Screen):
 
 	eventplayers = ListProperty('')
+	eventname = StringProperty('')
+	eventdate = StringProperty('')
+	
+	pass
+
+class YourEvents(Screen):
+
+	yourevents = ListProperty('')
+
+	pass
+
+class YourNotifications(Screen):
+
+	yournotifications = ListProperty('')
+
+	@mainthread
+	def on_enter(self):
+		for i, event in enumerate(self.yournotifications):
+			print(event)
+			event = Button(text= 'Join ' + event,pos=(100, 100+(i*50)), size_hint=(0.5,0.06))
+			accept = Button(text='Accept',pos=(350, 100+(i*50)),size_hint=(0.1,0.06))
+			reject = Button(text= 'Reject',pos=(400, 100+(i*50)),size_hint=(0.1,0.06))
+			self.ids.seeyournotifications.add_widget(event)	
+			self.ids.seeyournotifications.add_widget(accept)
+			self.ids.seeyournotifications.add_widget(reject)
+	pass
+
+class InvitePlayers(Screen):
+
+	username = StringProperty('')
+	team = StringProperty('')
+
+	def invite_player(self,invitor,team,invited):
+
+		url = 'http://127.0.0.1:5000/invite_player'
+		obj = {'invitor':invitor, 'invited':invited, 'team':team}
+		requests.post(url=url, data=obj)
+	
 	pass
 
 kv = Builder.load_file('app.kv')
