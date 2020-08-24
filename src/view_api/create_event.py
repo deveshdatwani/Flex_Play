de8ee32a033e80ec97a3d62cd6734a343198f2e3
deveshdatwani <datwanidevesh@gmail.com>
2020-08-25 04:47:06 +0530

    mvp version coming up

--- src/view_api/create_event.py
+++ src/view_api/create_event.py
@@ -5,32 +5,37 @@ import mysql.connector
 
 bp = Blueprint("create_event", __name__)
 
-@bp.route('/create_event', methods=['GET','POST'])
+@bp.route('/player_home/groups/group_home/create_group_event', methods=['GET','POST'])
 def create_event():
 
     if request.method == "POST":
         creater = request.form['creater']
-        player = request.form['player']
-        daytime = request.form['daytime']
-        eventarena = request.form['eventarena']
+        username = request.form['username']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        event_arena = request.form['eventarena']
+        play_time = request.form['playtime']
         latitude = request.form['latitude']
         longitude = request.form['longitude']
-        team = request.form['team']
-        privacy = request.form['privacy']
-        gameplaytime = request.form['gameplaytime']
+        privacy = 1
+        eventid = random.randit(100000,999999)
+        date_time = request.form['daytime']
         connector = mysql.connector.connect(host='us-cdbr-east-02.cleardb.com',user='be87857da36e44',password='665f4638',database='heroku_26cfe5af0cd58f4')
     
         if connector.is_connected():
             cursor = connector.cursor(buffered=True)
-            query = "INSERT INTO events_master(creater, player, daytime, eventarena, lat, lng, team, privacy, gameplaytime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);" 
-            val = creater, player, daytime, eventarena, latitude, longitude, team, privacy, gameplaytime
+            query = "INSERT INTO events_master (creater, eventarena, playtime, latitude, longitude, privacy, eventid, daytime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);" 
+            val = creater, even_tarena, playtime, latitude, longitude, privacy, eventid, date_time
             cursor.execute(query, val)
             connector.commit() 
-            response = {'response':'Created Event Successfully'}
+            query = "INSERT INTO events_players (username, firstname, lastname, eventid) VALUES (%s %s, %s, %s)"
+            val = username, firstname, lastname, eventid
+            cursor.execute(query, val)
+            connector.commit()
             cursor.close()
             connector.close()
+            response = 'response':'Created Event Successfully'
         else:
-            response = {'response':'something went wrong'}
-       
+            response = 'response':'something went wrong'
 
     return response
